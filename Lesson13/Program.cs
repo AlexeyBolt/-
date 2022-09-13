d58d8c59eb45a93fee7bebec42e7d72516ce28f6
AlexeyBolt <boltava.av@gmail.com>
2022-09-13 23:09:19 +0300

    Добавил схему Random

--- Lesson13/Program.cs
+++ Lesson13/Program.cs
@@ -1,4 +1,8 @@
-﻿int numberA = 3;
-int numberB = 8;
+﻿int numberA = new Random().Next(1, 5); // 1,2,3,4 max= max-1
+Console.WriteLine(numberA);
+Console.WriteLine("+");
+int numberB = new Random().Next(1, 100);
+Console.WriteLine(numberB);
+Console.WriteLine("=");
 int result = numberA + numberB;
 Console.WriteLine(result);
