5b7a8742ea8bbe090a7476a88079f30d4255001a
AlexeyBolt <boltava.av@gmail.com>
2022-09-14 00:21:22 +0300

    Добавил if else

---
+++ Lesson15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "господин")
+{
+    Console.WriteLine("Добро пожаловать, повелитель!");
+}
+else
+{
+    Console.Write("Беги,");
+    Console.WriteLine(username);
+    Console.Write("Беги!");
+}
